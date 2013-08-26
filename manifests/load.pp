23330cb1128c1f3f485a83ce832e21f9158024ee
jv <jeff@jeffvier.com>
2013-08-26 14:12:57 -0700

    better module persistence handling

--- manifests/load.pp
+++ manifests/load.pp
@@ -1,34 +1,48 @@
-define modprobe::load(
+define modprobe::load (
   $ensure = 'present'
 ){
 
-  file_line{"module_${name}":
-    line => $::operatingsystem ? {
-      debian => $name,
-      ubuntu => $name,
-      default => "/sbin/modprobe $name"
-    },
-    path => $::operatingsystem ? {
-      debian => '/etc/modules',
-      ubuntu => '/etc/modules',
-      default => '/etc/rc.modules'
-    },
-    ensure => $ensure,
+  $modulesfile = $::operatingsystem ? {
+        debian  => '/etc/modules',
+        ubuntu  => '/etc/modules',
+        default => '/etc/rc.modules'
+  }
+
+  file_line {
+    "module_${name}":
+      ensure  => $ensure,
+      path    => $modulesfile,
+      line    => $::operatingsystem ? {
+        debian  => $name,
+        ubuntu  => $name,
+        default => "/sbin/modprobe $name"
+      },
+      require => File[$modulesfile];
+  }
+
+  file {
+    $modulesfile:
+      ensure => present,
+      mode   => $::operatingsystem ? {
+        debian  => 0644,
+        ubuntu  => 0644,
+        default => 0755,
+      };
   }
   
   case $ensure {
     present: {
-      exec{"/sbin/modprobe ${name}":
+      exec {"/sbin/modprobe ${name}":
         unless => "/bin/grep -q '^${name} ' /proc/modules" 
       }
     }
     absent: {
-      exec{"/sbin/modprobe -r ${name}":
+      exec {"/sbin/modprobe -r ${name}":
         onlyif => "/bin/grep -q '^${name} ' /proc/modules"
       }
     }
     default: {
-      fail("Unknown ensure value '$ensure' for modprobe::kern_module!")
+      fail("Unknown ensure value '$ensure' for modprobe::load!")
     }
   }
 }
